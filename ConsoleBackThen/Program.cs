af733759798c627fc66271adcff50a21eedfb746
fred <domaine75@gmail.com>
2024-10-06 23:19:16 +0200

    Add text

--- ConsoleBackThen/Program.cs
+++ ConsoleBackThen/Program.cs
@@ -8,7 +8,8 @@ namespace ConsoleBackThen
     {
       Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("console back then");
-      Console.WriteLine("a long time ago I discovered first computer with this green color");
+      Console.WriteLine("a long time ago, 1980 or so, I discovered first computer with this green color");
+      Console.WriteLine("and everything was this color and I was spending hours developing with Basic a snake program");
       Console.WriteLine();
       Console.ForegroundColor = ConsoleColor.DarkYellow;
       Console.WriteLine("And then it was this yellowish color");
