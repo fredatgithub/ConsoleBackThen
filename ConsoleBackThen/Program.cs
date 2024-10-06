8e3c03dee84bd46edb7f31269ef1a8fc5bcc052e
fred <domaine75@gmail.com>
2024-10-06 23:16:36 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleBackThen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleBackThen
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine("console back then");
+      Console.WriteLine("a long time ago I discovered first computer with this green color");
+      Console.WriteLine();
+      Console.ForegroundColor = ConsoleColor.DarkYellow;
+      Console.WriteLine("An then it was this yellowish color");
+      Console.WriteLine("press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
